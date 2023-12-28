0a821c5e3d59155f067622dfb080e1908b31e9ce
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-28 16:00:51 -0300

    Updates

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('push', function(event) {
-  const body = event.data ? event.data.text() : 'No payload'
-  event.waitUntil(
-    self.registration.showNotification('Notificação', {
-      body
-    })
-  )
-  // console.log('Received a push message', event)
-})
