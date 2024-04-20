3bce8e3aa463375613e71ba0aac6b9cc4769e42f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-04-20 11:24:44 -0300

    Implementing carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,67 +1,69 @@
-import { useRef } from 'react';
 import { 
   Card, 
-  CarouselContainer, 
-  // ArrowsWrapper, 
   ImgContainer,
   TagsContainer,
-  CarouselWrapper } from './style';
-import {ArrowCircleRight, ArrowCircleLeft } from '@phosphor-icons/react'
+} from './style';
 import data from '../../data/data'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { 
+  EffectCoverflow,
+  Navigation, 
+  Pagination, 
+  } from 'swiper/modules';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function Carousel() {
 
-  const carouselRef = useRef<HTMLUListElement>(null);
-
-  const handleLeftArrow = () => { 
-    if (carouselRef.current) {
-      carouselRef.current.scrollLeft -= carouselRef.current.offsetWidth + 20;
-    }
-  }
-
-  const handleRightArrow = () => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollLeft += carouselRef.current.offsetWidth + 20;
-    }
-  }
-
   return (
     <>
-    <CarouselWrapper>
-      <CarouselContainer ref={carouselRef}>
+    <Swiper
+        modules={[EffectCoverflow, Navigation, Pagination]}
+        speed={1000}
+        slidesPerView={5}
+        centeredSlides
+        tag="section"
+        loop={true}
+        effect='coverflow'
+        coverflowEffect={{
+          rotate: 50, 
+          stretch: 0, 
+          depth: 160, 
+          modifier: 1, 
+          slideShadows: false,
+          scale: 0.7
+        }}
+        navigation
+        pagination={{ clickable: true }}
+      >
         {data.map((project) => (
-          <Card key={project.id}>
-            <a href={project.projectUrl} target="_blank">
-              <ImgContainer>
-                <img src={project.projectImage} alt="img" />
-              </ImgContainer>
-              <h3>{project.projectName}</h3>
-              <p>{project.projectDescription}</p>
-              <TagsContainer>
-                {project.tags.map((tag) => (
-                  <span key={tag}>{tag}</span>
-                ))}
-              </TagsContainer>
-            </a>
-          </Card>
+          <SwiperSlide
+            key={ project.id }
+          >
+            <Card>
+              <a href={project.projectUrl} target="_blank">
+                <ImgContainer>
+                  <img src={project.projectImage} alt="img" />
+                </ImgContainer>
+                <h3>{project.projectName}</h3>
+                <p>{project.projectDescription}</p>
+                <TagsContainer>
+                  {project.tags.map((tag) => (
+                    <span key={tag}>{tag}</span>
+                  ))}
+                </TagsContainer>
+              </a>
+            </Card>
+          </SwiperSlide>
         ))}
-        
-      </CarouselContainer>
-      {/* <ArrowsWrapper> */}
-      <button
-        type="button"
-        onClick={ handleLeftArrow }
-      >
-        <ArrowCircleLeft size={40}  weight="fill" />
-      </button>
-      <button
-        type='button'
-        onClick={ handleRightArrow }
-      >
-        <ArrowCircleRight size={40}  weight="fill" />
-      </button>
-      {/* </ArrowsWrapper> */}
-    </CarouselWrapper>
+      </Swiper>
+   
     </>
   );
 }
