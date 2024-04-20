f57b0056b222f05278370d0df541316d3810b895
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-04-20 15:52:37 -0300

    creating skills types

--- src/type.ts
+++ src/type.ts
@@ -2,3 +2,18 @@ export type ContextTheme = {
   theme: 'dark' | 'light';
   updateTheme: (theme: 'dark' | 'light') => void;
 }
+
+type SummariedSkill = {
+  title: string;
+  summarie: string;
+}
+
+type Skill = {
+  skillName: string;
+  skillPercent: number;
+}
+
+export type SkillsSectionData = {
+  summariesSkills: SummariedSkill[];
+  skills: Skill[];
+}
