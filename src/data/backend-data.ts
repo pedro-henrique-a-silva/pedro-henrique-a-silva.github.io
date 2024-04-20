2bbe2cc03bcbfcfad0ed6faf4fa9c4bdee83a90e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-04-20 15:55:26 -0300

    skills data

---
+++ src/data/backend-data.ts
@@ -0,0 +1,65 @@
+export default {
+  summariesSkills: [
+    {
+      title: "API Restful",
+      summarie: ` Dominio na criação de APIs web seguindo o padrão REST, 
+      utilizando frameworks como Express e Nest. A implementação de rotas e aplicações CRUD, 
+      demonstra uma habilidade essencial para desenvolver APIs robustas e escaláveis`
+    },
+    {
+      title: "Banco de Dados",
+      summarie: `Expertise na implementação de bancos de dados SQL, incluindo a normalização de dados para garantir 
+      eficiência e integridade. Desenvolvi habilidades sólidas na elaboração de consultas SQL, permitindo a extração de 
+      informações relevantes e a otimização do desempenho do sistema. Com esse conhecimento, estou apto a projetar e manter 
+      estruturas de dados robustas e eficazes para diversas aplicações.`
+    },
+    {
+      title: "Testes Unitários e de Integração",
+      summarie: `Possuo habilidades em Test-Driven Development (TDD) e um conhecimento sólido no desenvolvimento de testes 
+      unitários e de integração. Reconheço a importância dessas práticas no desenvolvimento de aplicações, 
+      garantindo a qualidade do código e a integridade do sistema. Com experiência em escrever testes eficazes, 
+      estou apto a identificar e corrigir problemas de forma ágil, contribuindo para a entrega de software confiável e 
+      de alto desempenho.`
+    },
+    {
+      title: "Node.js",
+      summarie: `
+      Possuo um conhecimento sólido na linguagem Node.js, especialmente na construção de aplicações backend. Utilizo todo o 
+      poder do TypeScript para desenvolver aplicações mais confiáveis, aproveitando os benefícios de tipagem estática para 
+      evitar erros comuns durante o desenvolvimento. Com experiência em Node.js, estou capacitado a criar sistemas robustos e 
+      escaláveis, atendendo às demandas de projetos complexos e de alto desempenho.
+      `
+    },
+    {
+      title: "Java",
+      summarie: `
+      Possuo um conhecimento sólido na construção de APIs utilizando a linguagem Java e o framework Spring. 
+      Com experiência em desenvolvimento Java, estou apto a criar APIs robustas e escaláveis, aproveitando os 
+      recursos oferecidos pelo Spring para facilitar o desenvolvimento e manutenção de aplicações. 
+      Meu domínio nessa tecnologia me permite oferecer soluções confiáveis e de alta qualidade para diversos projetos.
+    `
+    }
+  ],
+  skills: [
+    {
+      skillName: "API Restful",
+      skillPercent: 91
+    },
+    {
+      skillName: "Banco de Dados",
+      skillPercent: 75
+    },
+    {
+      skillName: "Node.js",
+      skillPercent: 88
+    },
+    {
+      skillName: "Java",
+      skillPercent: 75
+    },
+    {
+      skillName: "Testes",
+      skillPercent: 78
+    }
+  ]
+}
