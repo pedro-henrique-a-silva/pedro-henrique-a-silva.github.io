0e7287aa9fcd22015ecf917db09eb3fef2742730
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-06-29 17:51:48 -0300

    creating prop types for styled components

--- src/type.ts
+++ src/type.ts
@@ -1,6 +1,20 @@
-export type ContextTheme = {
-  theme: 'dark' | 'light';
-  updateTheme: (theme: 'dark' | 'light') => void;
+export type TitleColorType = '#E8E7E7' | '#FFC25C';
+export type SocialColorType = '#0077B5' | '#7232bd';
+
+export type TitleProps = {
+  titleColor: TitleColorType;
+  titleSize: number;
+  titleWeight: number;
+  isVisible: boolean;
+}
+
+export type AboutSocialMediaProps = {
+  socialColor: SocialColorType;
+}
+
+export type SkillsCardProps = {
+  cardDelay: number;
+  isVisible: boolean;
 }
 
 type SummariedSkill = {
