986f371ff29e1c3167eacb9cc7f019d37db7e2fc
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-06-29 17:53:05 -0300

    create paragraph components

---
+++ src/components/ui/paragraph.ts
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+export const Paragraph = styled.p`
+  font-size: .8rem;
+  font-weight: 400;
+  color: #656773;
+  transition: all 300ms ease;
+  font-family: 'Poppins', sans-serif;
+  margin-bottom: 0.5rem;
+`;
