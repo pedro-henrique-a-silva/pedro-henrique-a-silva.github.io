26be3c0146a588da45ddbe3bdfe346b82f7958bb
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-06-29 17:52:19 -0300

    creating header components

---
+++ src/components/ui/headerComponents.ts
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+
+export const HeaderBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 100px;
+  width: 100%;
+  transition: all 300ms ease;
+`;
