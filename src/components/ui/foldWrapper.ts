3c0a550b7156dce5a2bcfce8ea02b41832ca79ec
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-06-29 17:49:32 -0300

    creating fold wrapper component

---
+++ src/components/ui/foldWrapper.ts
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+
+export const FoldWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 100px 100px;
+  width: 100%;
+  background-color: #252734;
+  transition: all 300ms ease;
+`;
