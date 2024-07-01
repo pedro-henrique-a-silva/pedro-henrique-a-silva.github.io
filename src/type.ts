11e0de3179a1b3213027cd47ae8e0600280db840
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-01 12:52:28 -0300

    creating new prop types

--- src/type.ts
+++ src/type.ts
@@ -1,33 +1,54 @@
 export type TitleColorType = '#E8E7E7' | '#FFC25C';
 export type SocialColorType = '#0077B5' | '#7232bd';
 
+type Project = {
+  id: number,
+  projectName: string,
+  projectDescription: string,
+  projectImage: string,
+  projectUrl: string,
+  tags: string[]
+}
+
 export type TitleProps = {
-  titleColor: TitleColorType;
-  titleSize: number;
-  titleWeight: number;
-  isVisible: boolean;
+  $titleColor: TitleColorType;
+  $titleSize: number;
+  $titleWeight: number;
+  $isVisible: boolean;
 }
 
 export type AboutSocialMediaProps = {
-  socialColor: SocialColorType;
+  $socialColor: SocialColorType;
+}
+
+export type ProjectsSectionProps = {
+  projects: Project[];
+}
+
+export type ProjectCardSocialMediaProps = {
+  $socialColor: SocialColorType;
 }
 
 export type SkillsCardProps = {
-  cardDelay: number;
-  isVisible: boolean;
+  $cardDelay: number;
+  $isVisible: boolean;
 }
 
-type SummariedSkill = {
-  title: string;
-  summarie: string;
+export type FilterButtonProps = {
+  $isActived: boolean;
 }
 
-type Skill = {
-  skillName: string;
-  skillPercent: number;
+export type ParagraphProps = {
+  $paragraphColor?: string;
 }
 
-export type SkillsSectionData = {
-  summariesSkills: SummariedSkill[];
-  skills: Skill[];
+export type FilterMenuProps = {
+  setFilter: (filter: string) => void;
+  filter: string
 }
+
+export type CarouselProps = {
+  filter: string;
+  projects: Project[];
+}
+
