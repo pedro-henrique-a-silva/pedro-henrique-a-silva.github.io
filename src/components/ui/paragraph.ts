7c047a43a50bd751dbf2d047142fce3db611cc90
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-01 12:54:39 -0300

    refactor paragraph component

--- src/components/ui/paragraph.ts
+++ src/components/ui/paragraph.ts
@@ -1,9 +1,10 @@
 import { styled } from 'styled-components';
+import { ParagraphProps } from '../../type';
 
-export const Paragraph = styled.p`
+export const Paragraph = styled.p<ParagraphProps>`
   font-size: .8rem;
   font-weight: 400;
-  color: #656773;
+  color: ${({ $paragraphColor = "#656773"}) => $paragraphColor };
   transition: all 300ms ease;
   font-family: 'Poppins', sans-serif;
   margin-bottom: 0.5rem;
