ce48c874439c0bd7aee1f372d1002469e6aa6662
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-02 13:22:26 -0300

    implementando component app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import About from './components/AboutSection'
 import HeaderSection from './components/HeaderSection'
 import ProjectsSection from './components/ProjectsSection'
@@ -6,19 +5,41 @@ import SkillsSection from './components/SkillsSection'
 import { FoldWrapper } from './components/ui/foldWrapper'
 import data from './data/data'
 import GlobalStyles from './styles/global-styles'
+import FooterSection from './components/FooterSection'
+import { SkillsDetails, SkillsDetailsClose, SkillsDetailsWrapper, TitleDetail } from './components/ui/SkillsComponents'
+import { useState } from 'react'
+import skillsData from './data/skillsData'
+import { skillDetailType } from './types'
+import { ParagraphDetails } from './components/ui/paragraph'
+import { X } from '@phosphor-icons/react'
+
 
 
 function App() {
-  const [projects, setProjects] = useState(data)
+  const [skillDetail, setSkillDetail] = useState<skillDetailType>('');
   return (
     <>
       <GlobalStyles />
-      <HeaderSection/>
+      <HeaderSection />
       <About />
       <FoldWrapper>
-        <SkillsSection/>
+        <SkillsSection skillDetail={skillDetail} setSkillDetail={setSkillDetail}/>
       </FoldWrapper>
-      <ProjectsSection projects={projects} />
+      <ProjectsSection projects={data} />
+      <FooterSection/>
+      {skillDetail !== '' && <>
+      <SkillsDetailsWrapper></SkillsDetailsWrapper>
+      <SkillsDetails>
+        <TitleDetail $isVisible={true} $titleSize={1.5} $titleWeight={400} $titleColor="#E8E7E7">
+          {skillsData[skillDetail].title}
+        </TitleDetail>
+        <ParagraphDetails>{skillsData[skillDetail].summarie}</ParagraphDetails>
+        <SkillsDetailsClose onClick={() => setSkillDetail('')}>
+          <X size={32} />
+          </SkillsDetailsClose>
+      </SkillsDetails>
+        </>
+      }
     </>
 
   )
