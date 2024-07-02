7bc43a352089d4c9e1c6f1a3ec23dba81f7b2f6b
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-02 13:29:09 -0300

    implementando fonte dos dados

--- src/data/data.ts
+++ src/data/data.ts
@@ -5,6 +5,7 @@ export default [
     projectDescription: 'Nesse projeto foi desenvolvido uma loja online, com a possibilidade de adicionar produtos ao carrinho, remover produtos do carrinho, aumentar e diminuir a quantidade de produtos no carrinho, e também a possibilidade de filtrar os produtos por categoria, tudo isso utilizando.',
     projectImage: 'online-store.png',
     projectUrl: 'https://pedro-henrique-a-silva.github.io/online-store/',
+    repoUrl:'https://github.com/pedro-henrique-a-silva/online-store',
     tags: ["frontend"]
   },
   {
@@ -13,6 +14,7 @@ export default [
     projectDescription: 'Esse projeto pessoal foi um projeto inspirado em algums programas que ensinavam a digitar de forma mais rapida e precisa, que conheci quando começei minha história com computadores',
     projectImage: 'lehdi.png',
     projectUrl: 'https://pedro-henrique-a-silva.github.io/lehdi/',
+    repoUrl:'https://github.com/pedro-henrique-a-silva/lehdi',
     tags: ["frontend"]
   },
   {
@@ -21,6 +23,7 @@ export default [
     projectDescription: 'Nesse projeto foi desenvolvido um gerenciador de senhas, com a possibilidade de adicionar, remover e editar senhas, fazendo a validação das senhas antes que podessem ser salvas.',
     projectImage: 'password-manager.png',
     projectUrl: 'https://pedro-henrique-a-silva.github.io/password-manager/',
+    repoUrl:'https://github.com/pedro-henrique-a-silva/password-manager',
     tags: ["fronend"]
   },
   {
@@ -29,6 +32,7 @@ export default [
     projectDescription: 'Esse projeto é uma aplicação de previsão do tempo, onde é possível ver a previsão do tempo de qualquer cidade do mundo, aqui tive que utilizar o fetch para recuperar as informações de uma api',
     projectImage: 'ichoveu.png',
     projectUrl: 'https://pedro-henrique-a-silva.github.io/iChoveu/',
+    repoUrl:'https://github.com/pedro-henrique-a-silva/iChoveu',
     tags: ["frontend"]
   },
   {
@@ -37,6 +41,7 @@ export default [
     projectDescription: 'Esse projeto é uma aplicação de músicas com a possibilidade de adicionar músicas aos favoritos, remover músicas dos favoritos e editar o perfil do usuário.',
     projectImage: 'music-tunes.png',
     projectUrl: 'https://pedro-henrique-a-silva.github.io/musictunes/',
+    repoUrl:'https://github.com/pedro-henrique-a-silva/musictunes',
     tags: ["frontend"]
   },
   {
@@ -45,6 +50,7 @@ export default [
     projectDescription: 'Esse projeto é uma aplicação de testes utilizando a biblioteca de testes React Testing Library, para garantir o funcionamento correto da aplicação.',
     projectImage: 'react-testing-library.png',
     projectUrl: 'https://pedro-henrique-a-silva.github.io/react-testing-library/',
+    repoUrl:'https://github.com/pedro-henrique-a-silva/react-testing-library',
     tags: ["frontend"]
   },
   {
@@ -53,6 +59,7 @@ export default [
     projectDescription: 'Esse projeto é uma aplicação de gerenciamento de gastos, que já faz a conversão de várias moedas para o Real Brasileiro, e também é possível ver o saldo atual.',
     projectImage: 'my-wallet.png',
     projectUrl: 'https://pedro-henrique-a-silva.github.io/my-wallet',
+    repoUrl:'https://github.com/pedro-henrique-a-silva/my-wallet',
     tags: ["frontend"]
   },
   {
@@ -61,6 +68,7 @@ export default [
     projectDescription: 'Essa é a implementação de uma aplicação fullstack de receitas, com duas versões de backend utilizando Express e NestJS, respectivamente. O projeto também inclui uma aplicação frontend que consome os serviços dos backends para exibir receitas de comidas e bebidas',
     projectImage: 'backend-app-receitas.png',
     projectUrl: 'https://github.com/pedro-henrique-a-silva/receitas-fullstack',
+    repoUrl:'https://github.com/pedro-henrique-a-silva/receitas-fullstack',
     tags: ["frontend", "backend"]
   },
   {
@@ -69,6 +77,7 @@ export default [
     projectDescription: 'A aplicação backend para gerenciar dados de um campeonato de futebol. Com ela, é possível finalizar partidas, registrar resultados e atualizar o leaderboard automaticamente. Implementado usando POO e SOLID para ter uma estrutura robusta e eficiente, garantindo a confiabilidade e a escalabilidade do sistema.',
     projectImage: 'backend-api-futebol.png',
     projectUrl: 'https://github.com/pedro-henrique-a-silva/futbol-api',
+    repoUrl:'https://github.com/pedro-henrique-a-silva/futbol-api',
     tags: ['backend']
   },
   {
@@ -77,6 +86,7 @@ export default [
     projectDescription: 'A API é um sistema de gerenciamento de vendas em que será possível criar, visualizar, deletar e atualizar produtos e vendas. Você deverá utilizar o banco de dados MySQL para a gestão de dados.',
     projectImage: 'backend-manager-store.png',
     projectUrl: 'https://github.com/pedro-henrique-a-silva/manager-store',
+    repoUrl:'https://github.com/pedro-henrique-a-silva/manager-store',
     tags: ['backend']
   },
 ]
