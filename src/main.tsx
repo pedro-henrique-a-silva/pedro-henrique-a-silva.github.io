774a7d782f0180d81aa219a981b6f355d0908658
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-02 13:22:35 -0300

    removendo context

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import ContextPovider from './context/ContextPovider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ContextPovider>
-      <App />
-    </ContextPovider>,
+      <App />,
 )
