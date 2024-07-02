101c216c98123a4783d2aa343d86c6d04c2033d6
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-02 13:26:04 -0300

    refatorando header components

--- src/components/ui/headerComponents.ts
+++ src/components/ui/headerComponents.ts
@@ -8,4 +8,23 @@ export const HeaderBar = styled.div`
   padding: 20px 100px;
   width: 100%;
   transition: all 300ms ease;
+  
+  @media screen and (max-width: 650px) {
+    padding: 1rem 2rem;
+  }
+`;
+
+export const ButtonDownloadCV = styled.button`
+  background: transparent;
+  color: #FFC25C;
+  padding: 10px 20px;
+  border: none;
+  font-size: 1.2rem;
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 300ms ease;
+  
+  &:hover {
+    text-shadow: 0 0 5px #FFC25C;
+  }
 `;
