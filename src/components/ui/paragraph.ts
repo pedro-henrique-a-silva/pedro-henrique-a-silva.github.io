3cb327ac27c42afb84788014e1505961519a204f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-02 13:26:43 -0300

    refatorando paragraphs

--- src/components/ui/paragraph.ts
+++ src/components/ui/paragraph.ts
@@ -1,10 +1,21 @@
 import { styled } from 'styled-components';
-import { ParagraphProps } from '../../type';
+import { ParagraphProps } from '../../types/paragraphTypes';
 
 export const Paragraph = styled.p<ParagraphProps>`
   font-size: .8rem;
   font-weight: 400;
   color: ${({ $paragraphColor = "#656773"}) => $paragraphColor };
+  transition: all 300ms ease;
+  font-family: 'Poppins', sans-serif;
+  margin-bottom: 0.5rem;
+`;
+
+
+export const ParagraphDetails = styled.p<ParagraphProps>`
+  font-size: 0.8rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: ${({ $paragraphColor = "#E8E7E7"}) => $paragraphColor };
   transition: all 300ms ease;
   font-family: 'Poppins', sans-serif;
   margin-bottom: 0.5rem;
